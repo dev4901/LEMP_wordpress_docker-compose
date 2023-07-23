dffd035cbd78c9b6b06ab7907cd3155eb82dbeff
root <root@ip-172-31-26-136.us-east-2.compute.internal>
2023-07-23 19:44:38 +0000

    second

---
+++ script-test/test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! [ -z "$1" ]; then
+    echo "arg not given"
+else
+    echo "arg is given"
+
+fi
+
+# ls -la &> /dev/null
+# value="$?"
+# echo "$value"
+# if [ "$value" -ne 0 ]; then
+#         echo "value is not 0"
+#     else
+#         echo "value is 0 " 
+# fi
+
+# ls -la sdhjsgskh &> /dev/null
+# #value="$?"
+# echo $?
+# if (( $? )); then
+#         echo "value is not 0"
+#     else
+#         echo "value is 0 " 
+# fi
